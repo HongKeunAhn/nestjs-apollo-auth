f98e5207c33f91986ce16dbb35d202c54c3ee65e
Keu.ni <skcor8909@gmail.com>
2022-12-07 18:43:14 +0900

    <feat> auth service 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
+import { User } from './../users/models/user';
+import { UsersService } from './../users/users.service';
+import { JWT_SECRET } from './constants';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  validate(email: string, password: string): User | null {
+    const user = this.usersService.getUserByEmail(email);
+
+    if (!user) {
+      return null;
+    }
+
+    const passwordIsValid = password === user.password;
+    return passwordIsValid ? user : null;
+  }
+
+  login(user: User): { accessToken: string } {
+    const payload = {
+      email: user.email,
+      sub: user.userId,
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+
+  verify(token: string): User {
+    const decoded = this.jwtService.verify(token, {
+      secret: JWT_SECRET,
+    });
+
+    const user = this.usersService.getUserByEmail(decoded.email);
+
+    if (!user) {
+      throw new Error('Unable to get the user from decoded token.');
+    }
+
+    return user;
+  }
+}
