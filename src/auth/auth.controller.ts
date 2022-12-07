803ee717ecdf803da825797fd644edd985c5687a
Keu.ni <skcor8909@gmail.com>
2022-12-07 22:47:19 +0900

    <feat> add auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { User } from './../users/models/user';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Req() req: Request): { accessToken: string } {
+    return this.authService.login(req.user as User);
+  }
+}
