49357612017d64162049125334358c7519fb286c
Keu.ni <skcor8909@gmail.com>
2022-12-07 17:42:42 +0900

    <feat> provide user Module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
 
-@Module() 
+import { UsersService } from './users.service';
+import { UsersResolvers } from './users.resolver';
+
+@Module({
+  providers: [UsersResolvers, UsersService],
+})
 export class UsersModule {}
