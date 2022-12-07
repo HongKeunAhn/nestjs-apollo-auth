e66c26fa18dba0294576e8d94fafdbb6d303ce0a
Keu.ni <skcor8909@gmail.com>
2022-12-07 22:47:45 +0900

    <feat> set auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
+import { JwtAuthGuard, LocalAuthGuard } from './guard/';
+import { UsersModule } from './../users/users.module';
+import { JWT_SECRET } from './constants';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService],
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '3600s' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtAuthGuard, LocalAuthGuard],
 })
 export class AuthModule {}
